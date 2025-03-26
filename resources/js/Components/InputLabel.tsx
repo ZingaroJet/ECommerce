67bd3eaee1a193f9d45a62827f401a34b40cb044
ZingaroJet <dildaste@gmail.com>
2025-03-26 22:08:46 +0100

    DaisyUI

--- resources/js/Components/InputLabel.tsx
+++ resources/js/Components/InputLabel.tsx
@@ -1,20 +1,19 @@
 import { LabelHTMLAttributes } from 'react';
 
 export default function InputLabel({
-    value,
-    className = '',
-    children,
-    ...props
+  value,
+  className = '',
+  children,
+  ...props
 }: LabelHTMLAttributes<HTMLLabelElement> & { value?: string }) {
-    return (
-        <label
-            {...props}
-            className={
-                `block text-sm font-medium text-gray-700 ` +
-                className
-            }
-        >
-            {value ? value : children}
-        </label>
-    );
+  return (
+    <label
+      {...props}
+      className={
+        `fieldset` + className
+      }
+    >
+      <span className={"fieldset"}>{value ? value : children}</span>
+    </label>
+  );
 }
